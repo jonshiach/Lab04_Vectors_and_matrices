5a4321389937d6daf63f070c6f9a0e4df2b97a5b
Jon Shiach <jon.shiach@gmail.com>
2024-02-18 12:45:01 +0000

    First commit

--- source/main.cpp
+++ source/main.cpp
@@ -12,7 +12,7 @@ int main()
     std::cout << "Lab 4 Vectors and Matrices\n--------------------------\n" << std::endl;
     
     // Defining a vector
-    glm::vec3 u(2.0f, 2.0f, 1.0f);
+    glm::vec3 u = glm::vec3(2.0f, 2.0f, 1.0f);
     
     std::cout << "Defining a vector\n-----------------" << std::endl;
     std::cout << "u = " << u << std::endl;
